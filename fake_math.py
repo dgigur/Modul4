ee047c0802197318b6904d6f2e6532224a4be81a
dgigur <dgigurda005@mail.ru>
2024-07-29 18:30:34 +0300

    Урок 1

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        div_1 = first/second
+        return div_1
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        div_1 = first/second
+        return div_1
