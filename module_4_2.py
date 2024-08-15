658dc72de305cc37f5a1a8ef3facec9aa710ea23
dgigur <dgigurda005@mail.ru>
2024-08-15 17:47:51 +0300

    Урок 2

--- module_4_2.py
+++ module_4_2.py
@@ -4,5 +4,6 @@ def test_function():
     inner_function()
 
 
-#inner_function()
+#inner_function() Функция создается внутри другой функции которая не возвращает ничего
+#поэтому вне функции inner_function не существует
 test_function()
